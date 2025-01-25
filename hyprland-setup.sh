b1020a7b420a55792f21815857e9d6225a8ab502
lenistarr <marebrk@icloud.com>
2025-01-25 02:07:32 +0100

    Update hyprland-setup.sh

--- hyprland-setup.sh
+++ hyprland-setup.sh
@@ -10,15 +10,10 @@ rm -r ~/themes
 sudo cp -r ~/scripts/* /usr/local/bin/
 rm -r ~/scripts
 # sudo mv ~/vconsole.conf /etc/vconsole.conf
+# sudo mv ~/override.conf /etc/systemd/system/getty@tty1.service.d/
+# systemctl enable getty@tty1
 rm vconsole.conf LICENSE README.md
-sudo mv ~/override.conf /etc/systemd/system/getty@tty1.service.d/
-systemctl enable getty@tty1
 
 # Install programs
 sudo pacman -Syu
 sudo pacman -S brightnessctl cliphist fastfetch ffmpegthumbnailer file-roller firefox grim gvfs htop hyprland imv keepassxc kitty mako mpv network-manager-applet nm-connection-editor nwg-look pavucontrol polkit-gnome pipewire pipewire-pulse slurp swww tela-circle-icon-theme-purple thunar thunar-archive-plugin transmission-gtk ttf-font-awesome ttf-ibm-plex tumbler waybar wireplumber wlsunset wofi xdg-desktop-portal-hyprland yt-dlp zathura zathura-pdf-poppler 
-
-# Unmount USBs and reboot
-sudo umount -a
-sync
-reboot
