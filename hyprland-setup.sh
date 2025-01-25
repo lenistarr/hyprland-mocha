e634f07de2449039f1870c62dcb5b18865c25894
lenistarr <marebrk@icloud.com>
2025-01-25 02:29:21 +0100

    Update hyprland-setup.sh

--- hyprland-setup.sh
+++ hyprland-setup.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-# Basic home folder setup
-mkdir ~/documents ~/downloads ~/pictures ~/pictures/screenshots ~/pictures/wallpapers ~/videos
-cp -r ~/config/* ~/.config/
-rm -r ~/config
+# Basic setup and permissions
+sudo pacman -Syu
+sudo pacman -S git
+git clone https://github.com/lenistarr/hyprland-mocha.git
+cp ~/hyprland-mocha/.bash-profile ~/
+cp ~/hyprland-mocha/.bashrc ~/
+chmod +x ~/hyprland-mocha/scripts/*
+chmod +x ~/hyprland-mocha/.config/hypr/xdg-portal-hyprland 
 
-# Administration and customization
-sudo cp -r ~/themes/* /usr/share/themes/
-rm -r ~/themes
-sudo cp -r ~/scripts/* /usr/local/bin/
-rm -r ~/scripts
-# sudo mv ~/vconsole.conf /etc/vconsole.conf
-# sudo mv ~/override.conf /etc/systemd/system/getty@tty1.service.d/
+# Home folder setup
+mkdir ~/documents ~/downloads ~/pictures ~/pictures/screenshots ~/pictures/wallpapers ~/videos
+cp -r ~/hyprland-mocha/.config/* ~/.config/
+
+# Copy program and theme
+sudo cp -r ~/hyprland-mocha/themes/* /usr/share/themes/
+sudo cp -r scripts/* /usr/local/bin/
+# sudo mv ~/hyprland-mocha/vconsole.conf /etc/vconsole.conf
+# sudo mv ~/hyprland-mocha/override.conf /etc/systemd/system/getty@tty1.service.d/
 # systemctl enable getty@tty1
-rm vconsole.conf LICENSE README.md
+rm -r hyprland-mocha
 
 # Install programs
 sudo pacman -Syu
