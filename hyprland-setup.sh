c8b74ed4715b842e3303a064b7a91921363d7dfc
lenistarr <marebrk@icloud.com>
2025-01-26 13:04:23 +0100

    Update hyprland-setup.sh

--- hyprland-setup.sh
+++ hyprland-setup.sh
@@ -25,7 +25,7 @@ sudo systemctl enable getty@tty1
 
 # Install programs
 sudo pacman -Syu
-sudo pacman -S brightnessctl cliphist fastfetch ffmpegthumbnailer file-roller firefox grim gvfs htop hyprland imv keepassxc kitty mako mpv network-manager-applet nm-connection-editor nwg-look pavucontrol polkit-gnome pipewire pipewire-pulse slurp swww tela-circle-icon-theme-purple thunar thunar-archive-plugin transmission-gtk ttf-ibm-plex tumbler waybar wireplumber wlsunset wofi xdg-desktop-portal-hyprland yt-dlp zathura zathura-pdf-poppler
+sudo pacman -S brightnessctl cliphist fastfetch ffmpegthumbnailer file-roller firefox grim gvfs htop hyprland imv keepassxc kitty mako mpv network-manager-applet nm-connection-editor nwg-look pavucontrol polkit-gnome pipewire pipewire-pulse slurp swww tela-circle-icon-theme-purple thunar thunar-archive-plugin transmission-gtk ttf-ibm-plex ttf-font-awesome tumbler waybar wireplumber wlsunset wofi xdg-desktop-portal-hyprland yt-dlp zathura zathura-pdf-poppler
 
 # Script self delete
 sudo rm -r hyprland-mocha
