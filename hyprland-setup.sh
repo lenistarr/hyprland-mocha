2c3f4533b07ffd3287314fefe99f1106dd2e178f
lenistarr <marebrk@icloud.com>
2025-01-26 02:04:47 +0100

    Update hyprland-setup.sh

--- hyprland-setup.sh
+++ hyprland-setup.sh
@@ -12,6 +12,7 @@ chmod +x ~/hyprland-mocha/.config/hypr/xdg-portal-hyprland
 mv ~/hyprland-mocha/pictures ~/
 mkdir ~/documents ~/downloads ~/pictures/screenshots ~/videos
 mv ~/hyprland-mocha/.config ~/
+rm ~/.config/hypr/hyprland-nvidia.conf
 
 # Copy program and theme
 sudo mv ~/hyprland-mocha/themes/catppuccin-mocha-mauve /usr/share/themes/
