64ae8071b36b53d5d0db1ad84590e675d2f4ec47
lenistarr <marebrk@icloud.com>
2025-01-27 22:22:51 +0100

    Update hyprland-setup-nvidia.sh

--- hyprland-setup-nvidia.sh
+++ hyprland-setup-nvidia.sh
@@ -5,7 +5,7 @@ sudo pacman -S git
 git clone https://github.com/lenistarr/hyprland-mocha.git
 mv ~/hyprland-mocha/.bash_profile ~/
 mv ~/hyprland-mocha/.bashrc ~/
-chmod +x ~/hyprland-mocha/scripts/*
+chmod +x ~/hyprland-mocha/.local/bin/*
 chmod +x ~/hyprland-mocha/.config/hypr/xdg-portal-hyprland
 
 # Home folder setup
@@ -13,10 +13,11 @@ mv ~/hyprland-mocha/pictures ~/
 mkdir ~/documents ~/downloads ~/music ~/pictures/screenshots ~/videos
 mv ~/hyprland-mocha/.config ~/
 mv ~/hyprland-mocha/.local/share/applications ~/.local/share/
+mv ~/hyprland-mocha/.local/bin ~/.local/
+mv ~/hyprland-mocha/.themes ~/
 mv ~/.config/hypr/hyprland-nvidia.conf ~/.config/hypr/hyprland.conf
 
 # Copy programs and theme
-sudo mv ~/hyprland-mocha/scripts/* /usr/local/bin/
 sudo mkdir /etc/systemd/system/getty@tty1.service.d
 sudo mv ~/hyprland-mocha/override.conf /etc/systemd/system/getty@tty1.service.d/
 sudo systemctl enable getty@tty1
@@ -24,7 +25,6 @@ sudo systemctl enable getty@tty1
 # Install programs
 sudo pacman -Syu
 sudo pacman -S brightnessctl cliphist egl-wayland fastfetch ffmpegthumbnailer file-roller firefox grim gvfs htop hyprland imv keepassxc kitty libva-nvidia-driver linux-headers mako mpv network-manager-applet nm-connection-editor nvidia-open-dkms nvidia-utils nwg-look pavucontrol polkit-gnome pipewire pipewire-pulse slurp swww tela-circle-icon-theme-purple thunar thunar-archive-plugin transmission-gtk ttf-ibm-plex ttf-font-awesome tumbler waybar wireplumber wlsunset wofi xdg-desktop-portal-hyprland yt-dlp zathura zathura-pdf-poppler
-sudo mv ~/hyprland-mocha/themes/catppuccin-mocha-mauve /usr/share/themes/
 
 # Script self delete
 sudo rm -r hyprland-mocha
